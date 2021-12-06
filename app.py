7e4500869c2392f4df17db72473dfea87cf3bfbe
Zach <Zacaframe@gmail.com>
2021-12-06 00:42:17 -0500

    Second Commit

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:vj4cxex6@localhost:5432/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "chickenzrcoolz"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+
+@app.route('/')
+def home_page():
+    """Shows home page"""
+    return render_template("home.html")
+
+@app.route('/users')
+def users():
+    """Show All Users"""
+    return
+
+@app.route("/users/new")
+def add_new_user():
+    """Form for adding a new user"""
+    return render_template("new_user.html")
+
+@app.route("/users/new", methods=["POST"])
+def post_new_user():
+    """Post route for adding a new user. Redirect to /users."""
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    profile_url = request.form["profile_url"]
+
+    new_user = User(first_name=first_name, last_name=last_name, profile_url=profile_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
+    # return redirect(f"/{new_user.id}")
+
+# @app.route("/users/<int:user_id>")
+# def detailed_user_page():
+#     """Show info about a given user, edit or delete user."""
+
+# @app.route("/users/<int:user_id>/edit")
+# def edit_page():
+#     """Show us the edit page. Has a cancel button to return to the detail page, and a save button."""
+
+# @app.route("/users/<int:[user-id]>/edit", methods=["POST"])
+# def post_edit():
+#     """POSTS our edit, sends to server to update information."""
+
+# @app.route("/users/<int:user_id>/delete", methods=["POST"])
+# def post_delete():
+#     """DELETES our user, REDIRECTS to /users"""
--- models.py
+++ models.py
@@ -1,1 +1,32 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+# Models Go Below!
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        s = self
+        return
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False,
+                           unique=False)
+    last_name = db.Column(db.String(50),
+                          nullable=False,
+                          unique=False)
+    profile_url = db.Column(db.String(1000),
+                          nullable=True,
+                          unique=False,
+                          default="https://i.imgur.com/ST5Q7hj.png")
